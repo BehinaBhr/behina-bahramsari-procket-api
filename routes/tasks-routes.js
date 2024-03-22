77be26b5d2893102d21a2374616165234dd4356f
BehinaBhr <be.bhr77@gmail.com>
2024-03-22 02:52:31 -0700

    Implement 'add' function to handle POST requests for creating new tasks, performing validation for required fields, checking for existing tasks with the same description for a goal, and ensuring the existence of the associated goal.

--- routes/tasks-routes.js
+++ routes/tasks-routes.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 const tasksController = require('../controllers/tasks-controller');
 
-router.route("/").get(tasksController.list);
+router.route("/").get(tasksController.list).post(tasksController.add);
 router.route("/:id").get(tasksController.findOne);
 
 module.exports = router;
