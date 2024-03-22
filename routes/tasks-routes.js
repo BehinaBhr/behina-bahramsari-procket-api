a4650cb4de3908273061d98929176776b48f83b2
BehinaBhr <be.bhr77@gmail.com>
2024-03-22 03:01:25 -0700

    Implement remove function to DELETE a specific task using its ID

--- routes/tasks-routes.js
+++ routes/tasks-routes.js
@@ -2,6 +2,6 @@ const router = require('express').Router();
 const tasksController = require('../controllers/tasks-controller');
 
 router.route("/").get(tasksController.list).post(tasksController.add);
-router.route("/:id").get(tasksController.findOne);
+router.route("/:id").get(tasksController.findOne).delete(tasksController.remove);
 
 module.exports = router;
