bba0c1d1f11b6b4634bfe3ed2772e0d1d313585a
BehinaBhr <be.bhr77@gmail.com>
2024-03-22 01:09:58 -0700

    Implement findOne function to send a GET request for a single task from the endpoint, including goal description for the task with the given ID in the response

--- routes/tasks-routes.js
+++ routes/tasks-routes.js
@@ -2,5 +2,6 @@ const router = require('express').Router();
 const tasksController = require('../controllers/tasks-controller');
 
 router.route("/").get(tasksController.list);
+router.route("/:id").get(tasksController.findOne);
 
 module.exports = router;
