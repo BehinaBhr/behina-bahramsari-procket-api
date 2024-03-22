0b91bea1983406fbd73580f8b98a3dea9916fcb1
BehinaBhr <be.bhr77@gmail.com>
2024-03-21 23:09:52 -0700

    Implement findOne function to send a GET request for a single goal from the endpoint.

--- controllers/goals-controller.js
+++ controllers/goals-controller.js
@@ -1,18 +1,36 @@
 const knex = require("knex")(require("../knexfile"));
 
-//fields to select for goals
+// fields to select for goals
 const goalAttr = ["id", "goal_description", "start_date", "end_date"];
 
-//get list of warehouses
+// get list of goals
 const list = async (_req, res) => {
-    try {
-      const data = await knex("goals").select(goalAttr);
-      res.status(200).json(data);
-    } catch (err) {
-      res.status(400).send(`Error retrieving goals: ${err}`);
+  try {
+    const data = await knex("goals")
+    .select(goalAttr);
+    res.status(200).json(data);
+  } catch (err) {
+    res.status(400).send(`Error retrieving goals: ${err}`);
+  }
+};
+
+// get a single goal
+const findOne = async (req, res) => {
+  try {
+    const goal = await knex("goals")
+      .where({ id: req.params.id })
+      .select(goalAttr)
+      .first();
+    if (!goal) {
+      return res.status(404).json({ error: "Goal not found" });
     }
-  };
+    res.status(200).json(goal);
+  } catch (err) {
+    res.status(500).send(`Error retrieving goals: ${err}`);
+  }
+};
 
 module.exports = {
   list,
+  findOne,
 };
--- routes/goals-routes.js
+++ routes/goals-routes.js
@@ -2,5 +2,6 @@ const router = require("express").Router();
 const goalsController = require("../controllers/goals-controller");
 
 router.route("/").get(goalsController.list);
+router.route('/:id').get(goalsController.findOne);
 
 module.exports = router;
