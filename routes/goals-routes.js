c1acc0036560ab81392bb9d15b2ebd57842e8323
BehinaBhr <be.bhr77@gmail.com>
2024-03-22 00:02:26 -0700

    Implement remove function to DELETE a specific goal using its ID

--- routes/goals-routes.js
+++ routes/goals-routes.js
@@ -2,6 +2,6 @@ const router = require("express").Router();
 const goalsController = require("../controllers/goals-controller");
 
 router.route("/").get(goalsController.list).post(goalsController.add);;
-router.route('/:id').get(goalsController.findOne);
+router.route('/:id').get(goalsController.findOne).delete(goalsController.remove);
 
 module.exports = router;
