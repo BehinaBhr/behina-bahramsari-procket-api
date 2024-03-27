5e6ee7bba2a128f8faedffc924983a70788bc464
BehinaBhr <be.bhr77@gmail.com>
2024-03-26 18:14:24 -0700

    Change date and timestamp format in general

--- knexfile.js
+++ knexfile.js
@@ -9,5 +9,11 @@ module.exports = {
       user: process.env.DB_LOCAL_USER,
       password: process.env.DB_LOCAL_PASSWORD,
       charset: "utf8",
+            typeCast: function (field, next) {
+              if (field.type == 'DATE' || field.type == 'TIMESTAMP') {
+                return field.string();
+              }
+              return next();
+            }
     },
   };
