c3a4677b495f89574b5b4d61ea5125d6558566ac
BehinaBhr <be.bhr77@gmail.com>
2024-03-30 18:25:47 -0700

    Add past and ongoing to tasks-routs

--- routes/tasks-routes.js
+++ routes/tasks-routes.js
@@ -1,9 +1,11 @@
-const router = require('express').Router();
-const tasksController = require('../controllers/tasks-controller');
+const router = require("express").Router();
+const tasksController = require("../controllers/tasks-controller");
 
 router.route("/").get(tasksController.list).post(tasksController.add);
+router.route("/past").get(tasksController.past);
+router.route("/on-going").get(tasksController.onGoing);
 router.route("/:id").get(tasksController.findOne).delete(tasksController.remove).put(tasksController.update);
-router.route('/:id/procrastinations').get(tasksController.procrastinations);
-router.route('/:id/procrastinations/grouped').get(tasksController.procrastinations_grouped);
+router.route("/:id/procrastinations").get(tasksController.procrastinations);
+router.route("/:id/procrastinations/grouped").get(tasksController.procrastinations_grouped);
 
 module.exports = router;
