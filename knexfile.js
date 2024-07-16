18a3ec923687c2e2a6ede3df5a59b7f2f0aa9ef1
BehinaBhr <be.bhr77@gmail.com>
2024-07-16 16:08:55 -0700

    Refactor .env and knexfile

--- knexfile.js
+++ knexfile.js
@@ -1,33 +1,18 @@
 require("dotenv").config();
 
-//connect setup
 module.exports = {
-  // client: "mysql2",
-  // connection: {
-  //   host: process.env.DB_LOCAL_HOST,
-  //   database: process.env.DB_LOCAL_DBNAME,
-  //   user: process.env.DB_LOCAL_USER,
-  //   password: process.env.DB_LOCAL_PASSWORD,
-  //   charset: "utf8",
-  //         typeCast: function (field, next) {
-  //           if (field.type == 'DATE' || field.type == 'TIMESTAMP') {
-  //             return field.string();
-  //           }
-  //           return next();
-  //         }
-  // },
-   client: "mysql2",
+  client: "mysql2",
   connection: {
     host: process.env.DB_HOST,
     database: process.env.DB_DBNAME,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     charset: "utf8",
-          typeCast: function (field, next) {
-            if (field.type == 'DATE' || field.type == 'TIMESTAMP') {
-              return field.string();
-            }
-            return next();
-          }
-  },
+    typeCast: function (field, next) {
+      if (field.type == 'DATE' || field.type == 'TIMESTAMP') {
+        return field.string();
+      }
+      return next();
+    }
+  }
 };
